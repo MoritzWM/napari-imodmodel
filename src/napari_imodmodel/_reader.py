1fc5c360cf4939e192f8d7ce2cf8c75c5bf19a70
MoritzWM <mail@moritzwm.de>
2023-11-09 14:39:13 +0100

    Load mesh colors

--- src/napari_imodmodel/_reader.py
+++ src/napari_imodmodel/_reader.py
@@ -1,9 +1,13 @@
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Union
 from pathlib import Path
 
 import numpy as np
 
 from imodmodel import ImodModel
+
+if TYPE_CHECKING:
+    from typing import Iterable
+    from napari.types import LayerDataTuple
 
 
 def napari_get_reader(path: Union[str, List[str]]) -> Optional[Callable]:
@@ -14,22 +18,22 @@ def napari_get_reader(path: Union[str, List[str]]) -> Optional[Callable]:
         return None
     return reader_function
 
-def read_model(path: Path) -> List[Tuple[np.ndarray, np.ndarray]]:
+def read_model(path: Path) -> "Iterable[LayerDataTuple]":
     model = ImodModel.from_file(path)
     meshes = list()
-    for object in model.objects:
-        for mesh in object.meshes:
+    for obj in model.objects:
+        color = np.array([obj.header.red, obj.header.green, obj.header.blue])
+        for mesh in obj.meshes:
+            vertex_colors = np.broadcast_to(color, (len(mesh.vertices), 3))
             if mesh.face_values is None:
-                meshes.append((mesh.vertices, mesh.indices))
+                yield (mesh.vertices, mesh.indices), dict(vertex_colors=vertex_colors, shading="smooth"), "surface"
             else:
-                meshes.append((mesh.vertices, mesh.indices, mesh.face_values))
-    return meshes
+                yield (mesh.vertices, mesh.indices, mesh.face_values), dict(vertex_colors=vertex_colors, shading="smooth"), "surface"
 
 def reader_function(paths: Union[str, List[str]]) -> List[Tuple[Any, Dict, str]]:
     if not isinstance(paths, list):
         paths = [paths]
     layers = list()
     for path in paths:
-        for mesh in read_model(Path(path)):
-            layers.append((mesh, {}, "surface"))
+        layers += list(read_model(Path(path)))
     return layers
