603734c3873464278a3a9b9f2039a9f233575cf1
MoritzWM <mail@moritzwm.de>
2023-11-09 14:39:48 +0100

    Format with black

--- src/napari_imodmodel/_reader.py
+++ src/napari_imodmodel/_reader.py
@@ -1,4 +1,13 @@
-from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Union
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    Union,
+)
 from pathlib import Path
 
 import numpy as np
@@ -13,10 +22,11 @@ if TYPE_CHECKING:
 def napari_get_reader(path: Union[str, List[str]]) -> Optional[Callable]:
     if not isinstance(path, list):
         path = [path]
-    print('getting reader')
-    if not all(map(lambda p: p.endswith('.mod'), path)):
+    print("getting reader")
+    if not all(map(lambda p: p.endswith(".mod"), path)):
         return None
     return reader_function
+
 
 def read_model(path: Path) -> "Iterable[LayerDataTuple]":
     model = ImodModel.from_file(path)
@@ -26,11 +36,18 @@ def read_model(path: Path) -> "Iterable[LayerDataTuple]":
         for mesh in obj.meshes:
             vertex_colors = np.broadcast_to(color, (len(mesh.vertices), 3))
             if mesh.face_values is None:
-                yield (mesh.vertices, mesh.indices), dict(vertex_colors=vertex_colors, shading="smooth"), "surface"
+                yield (mesh.vertices, mesh.indices), dict(
+                    vertex_colors=vertex_colors, shading="smooth"
+                ), "surface"
             else:
-                yield (mesh.vertices, mesh.indices, mesh.face_values), dict(vertex_colors=vertex_colors, shading="smooth"), "surface"
+                yield (mesh.vertices, mesh.indices, mesh.face_values), dict(
+                    vertex_colors=vertex_colors, shading="smooth"
+                ), "surface"
 
-def reader_function(paths: Union[str, List[str]]) -> List[Tuple[Any, Dict, str]]:
+
+def reader_function(
+    paths: Union[str, List[str]]
+) -> List[Tuple[Any, Dict, str]]:
     if not isinstance(paths, list):
         paths = [paths]
     layers = list()
