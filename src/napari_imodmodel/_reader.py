61b16ddc74378213f3ddecb673906aeb60fe6c3a
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2024-04-10 13:31:37 +0200

    Also read contours and points

--- src/napari_imodmodel/_reader.py
+++ src/napari_imodmodel/_reader.py
@@ -19,6 +19,13 @@ if TYPE_CHECKING:
     from napari.types import LayerDataTuple
 
 
+def _obj_type(flags: int) -> str:
+    if flags & 1<<9: # bit 9
+        return "scattered"
+    elif flags & 1<<3: # bit 3
+        return "open"
+    return "closed"
+
 def napari_get_reader(path: Union[str, List[str]]) -> Optional[Callable]:
     if not isinstance(path, list):
         path = [path]
@@ -30,19 +37,37 @@ def napari_get_reader(path: Union[str, List[str]]) -> Optional[Callable]:
 
 def read_model(path: Path) -> "Iterable[LayerDataTuple]":
     model = ImodModel.from_file(path)
-    meshes = list()
-    for obj in model.objects:
+    for obj_num, obj in enumerate(model.objects, start=1):
         color = np.array([obj.header.red, obj.header.green, obj.header.blue])
-        for mesh in obj.meshes:
+        obj_type = _obj_type(obj.header.flags)
+        layer_data = [
+            contour.points[:, (2, 1, 0)]
+            for contour in obj.contours
+        ]
+        kwargs = {
+            "name": f"{path.stem} obj {obj_num}",
+            "edge_color": "#" + ''.join([f'{int(c*255):0>2x}' for c in color]),
+            }
+        if obj_type == "scattered":
+            layer_type = "points"
+            layer_data = np.concatenate(layer_data)
+        else:
+            layer_type = "shapes"
+            kwargs["shape_type"] = "path" if obj_type == "open" else "polygon"
+        yield layer_data, kwargs, layer_type
+
+        for mesh_num, mesh in enumerate(obj.meshes, start=1):
             vertex_colors = np.broadcast_to(color, (len(mesh.vertices), 3))
             if mesh.face_values is None:
-                yield (mesh.vertices[:, (2, 1, 0)], mesh.indices), dict(
-                    vertex_colors=vertex_colors, shading="smooth"
-                ), "surface"
+                mesh_data = (mesh.vertices[:, (2, 1, 0)], mesh.indices)
             else:
-                yield (mesh.vertices[:, (2, 1, 0)], mesh.indices, mesh.face_values), dict(
-                    vertex_colors=vertex_colors, shading="smooth"
-                ), "surface"
+                mesh_data = (mesh.vertices[:, (2, 1, 0)], mesh.indices, mesh.face_values)
+            mesh_kwargs = {
+                "name": f"{kwargs['name']} mesh {mesh_num}",
+                "vertex_colors": vertex_colors,
+                "shading": "smooth",
+            }
+            yield mesh_data, mesh_kwargs, "surface"
 
 
 def reader_function(
